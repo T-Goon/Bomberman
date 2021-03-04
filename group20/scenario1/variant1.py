6b9599f892519a0f72fc9438d0a01f6786334bd3
Timothy Goon <t.kgoonx@gmail.com>
2021-03-04 12:40:58 -0500

    renamed group folder

--- group20/scenario1/variant1.py
+++ group20/scenario1/variant1.py
@@ -7,7 +7,7 @@ sys.path.insert(1, '..')
 from game import Game
 
 # TODO This is your code!
-sys.path.insert(1, '../groupNN')
+sys.path.insert(1, '../group20')
 
 # Uncomment this if you want the empty test character
 #from testcharacter import TestCharacter
